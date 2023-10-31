fe6f0a6a03a576a857a64dccbf6b097fc2b1e086
Chad Scherrer <chad.scherrer@gmail.com>
2023-10-31 07:47:56 -0700

    Use a VecDeque

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-// use rayon::prelude::*;
+use std::collections::VecDeque;
 
+// A "filter" (nothing official here, just sounds good to me) is a sequence of
+// multiples of some prime. In the Sieve of Eratosthenes, it's the sequence of
+// "crossed out numbers" (p, 2p, 3p, ...) for any prime p.
+//
+// This could easily be made an Iterator, but we don't use that functionality so
+// we leave it out.
 struct Filter {
     base: u64,
-    state: u64
+    state: u64,
 }
 
 impl Filter {
+    // A new filter could naively start at p, but we can do better. We know that
+    // p^2 is the first number in the filter that is not a multiple of any
+    // smaller prime. So we start there.
     fn new(base: u64) -> Filter {
         Filter {
             base: base,
-            state: base * base
+            state: base * base,
         }
     }
 
@@ -26,39 +35,47 @@ impl Filter {
     }
 }
 
+// The Primes struct is an iterator over prime numbers. It maintains a list of
+// active filters, and a queue of filters (really a VecDeque) that are waiting
+// to be activated. 
+//
+// This queue is helpful because it's inefficient to constantly search a filter
+// we know won't be useful until we're at the square of its base
 struct Primes {
-    filters: Vec<Filter>,
-    state: u64
+    active_filters: Vec<Filter>,
+    queued_filters: VecDeque<Filter>,
+    state: u64,
 }
 
 impl Primes {
     fn new() -> Primes {
         Primes {
-            filters: Vec::new(),
-            state: 1
+            active_filters: Vec::new(),
+            queued_filters: VecDeque::new(),
+            state: 1,
         }
     }
 
     fn step(&mut self) -> Option<u64> {
         self.state += 1;
-        for f in self.filters.iter_mut() {
-            if f.query(self.state) {
-                return None;
-            }
+        
+        // If any active filter matches, we're not prime
+        if self.active_filters.iter_mut().any(|f| f.query(self.state)) {
+            return None;
         }
-        self.filters.push(Filter::new(self.state));
+
+        // If the next queued filter is ready, activate it
+        if self.state == self.queued_filters.front().map(|f| f.state).unwrap_or(0) {
+            self.active_filters
+                .push(self.queued_filters.pop_front().unwrap());
+            return None;
+        }
+
+        // If we reach this point, we know we're at a prime number. So queue a
+        // new filter and return a Some
+        self.queued_filters.push_back(Filter::new(self.state));
         Some(self.state)
     }
-
-    // // Here's an alternative to check filters in parallel using the rayon crate.
-    // fn step(&mut self) -> Option<u64> {
-    //     self.state += 1;
-    //     if self.filters.par_iter_mut().any(|f|{  f.query(self.state)}) {
-    //         return None;
-    //     }
-    //     self.filters.push(Filter::new(self.state));
-    //     Some(self.state)
-    // }
 }
 
 impl Iterator for Primes {
@@ -72,7 +89,7 @@ impl Iterator for Primes {
 
 fn main() {
     let primes = Primes::new();
-    for p in primes.skip(999).take(1) {
+    for p in primes.skip(999999).take(1) {
         println!("{}", p);
     }
 }
